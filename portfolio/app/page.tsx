1b19e18791b25e5f8abf9f4ff6f0930f50b8c07e
KetenZits <165859061+KetenZits@users.noreply.github.com>
2025-08-04 00:29:42 +0700

    .
    .

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -5,9 +5,17 @@ import TextType from "@/components/TextType";
 import Particles from "@/components/Particles";
 import {Code} from "@heroui/react";
 import Magnet from "@/components/Magnet";
-import RippleGrid from "@/components/RippleGrid";
+import {Card, CardHeader, CardBody, Image} from "@heroui/react";
+import Link from "next/link";
 
 export default function Home() {
+
+    const projectlist = [
+        {tiltedimg: "#", name1: "", count: "", name2: "", detail: ""},
+        {tiltedimg: "#", name1: "", count: "", name2: "", detail: ""},
+        {tiltedimg: "#", name1: "", count: "", name2: "", detail: ""},
+    ];
+
   return (
     <>
     <div style={{ width: '100%', height: '100%', position: 'absolute' }}>
@@ -175,6 +183,74 @@ export default function Home() {
         </div>
       </div>
       </div>
+    </section>
+    <div style={{ width: '100%', height: '100%', position: 'absolute' }}>
+        <Particles
+          particleColors={['#ffffff', '#8800C9']}
+          particleCount={1000}
+          particleSpread={40}
+          speed={0.075}
+          particleBaseSize={150}
+          alphaParticles={true}
+          disableRotation={false}
+        />
+      </div>
+    <section className="mt-3 flex flex-col items-center justify-center gap-4 md:py-10 h-screen bg-black" id="project">
+        <div className="container mx-auto text-center z-50 flex flex-col items-center justify-center">
+          <TextType 
+          text={["MY ALL PROJECTS"]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          showCursor={true}
+          cursorCharacter="_"
+          className="text-4xl sm:text-4xl md:text-6xl font-bold text-white text-center"
+        />
+        <Magnet padding={50} disabled={false} magnetStrength={10}>
+          <Code color="secondary" className="ring-purple-400 ring-1 text-sm md:text-md my-5 font-semibold bg-gradient-to-r from-pink-600 via-fuchsia-500 to-violet-600 bg-clip-text text-transparent">💫 ALL OF MY PROJECTS I DO</Code>
+        </Magnet>
+        <div className="flex flex-col my-5">
+          {/* ------------------ */}
+          <div className="flex flex-row flex-wrap gap-10 bg-gradient-to-r from-pink-600/10 via-fuchsia-500/10 to-violet-600/10 py-10 px-8 ring-2 ring-white/20 rounded-2xl shadow-lg">
+            <TiltedCard
+              imageSrc="/project/project1.png"
+              altText="Thanapon Khawkumkrong - Dev"
+              captionText="Thanapon Khawkumkrong - Dev"
+              containerHeight="300px"
+              containerWidth="575px"
+              imageHeight="300px"
+              imageWidth="575px"
+              rotateAmplitude={8}
+              scaleOnHover={1.07}
+              showMobileWarning={false}
+              showTooltip={true}
+              displayOverlayContent={true}
+              overlayContent={
+                <div className="backdrop-blur-lg bg-white/50 ring-2 ring-gray-800/50 py-1.5 px-2 rounded-xl shadow-lg relative top-5 left-3">
+                  <h1 className="text-[16px] font-bold bg-gradient-to-r from-pink-600 via-fuchsia-500 to-violet-600 bg-clip-text text-transparent transition-all duration-300">
+                    NBAC REPAIR SYSTEM ✨
+                  </h1>
+                </div>
+              }
+            />
+            <div className="flex flex-col items-start justify-center">
+              <span className={title({ color: "violet" })}>{"{"}PROJECT 1{"};"}&nbsp;</span>
+              <span className={title({ color: "blue" })}>NBAC Repair System&nbsp;</span>
+              <p className="mt-5 text-start text-sm font-normal w-100 bg-gradient-to-br from-cyan-600 via-teal-500 to-green-600 bg-clip-text text-transparent">
+                NBAC Repair System is a system developed for reporting computer repairs when damage occurs or when it's necessary to inspect the condition of individual computers in an organized manner. The system focuses on convenience, speed, and efficiency in managing computer equipment issues within the organization.</p>
+              <Link
+              href="#contact"
+              className="mt-5 bg-gradient-to-br from-purple-600 to-indigo-700 hover:from-purple-500 hover:to-indigo-600 text-white px-4 py-2 rounded-2xl shadow-[0_3px_24px_rgba(147,51,234,0.4)] hover:scale-105 transition font-medium flex justify-center items-center gap-2"
+            >
+              Source
+              <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+              </svg>
+            </Link>
+            </div>
+          </div>
+          {/* ------------------ */}    
+        </div>
+        </div>
     </section>
     </>
   );
